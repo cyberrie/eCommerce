a57193fa21a570578dab46befe9f9e672094a7a9
helena <helena.gilja@gmail.com>
2023-04-10 11:39:37 +0100

    global styles added, babelrc created, all the components created, exported from index.js, herobanner started

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroBanner = () => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
--- ecommerce/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: '6h81ipro',
-    dataset: 'production'
-  }
-})
