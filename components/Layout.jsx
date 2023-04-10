b512d63c98bccdcea0cbd3ee788007cc7393cb52
helena <helena.gilja@gmail.com>
2023-04-10 15:48:26 +0100

    layout created

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
-const Layout = () => {
+// wrapper for the whole page also containing metadata, navbar, main, footer etc..
+// pass children so it renders it in the main
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>Helena's Web Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+      {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
