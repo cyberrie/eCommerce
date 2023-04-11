3e53684feb1140d987a0615f6ea379328644dac0
helena <helena.gilja@gmail.com>
2023-04-11 10:57:43 +0100

    app wrapped into context

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import "@/styles/globals.css";
 import { Layout } from "@/components";
 import React from "react";
+import { StateContext } from "@/context/StateContext";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
   return (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-  )
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
-
