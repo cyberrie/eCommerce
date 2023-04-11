9a82bc25b6e788651e2f1f6f0ea77cefad7dd75c
helena <helena.gilja@gmail.com>
2023-04-11 11:46:42 +0100

    cart style added, hide/show logic added, empty/populated cart logic added, showing - and + but no functionality yet. Cart is rendering imgs and price of added products, delete btn added but no logic

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useRef } from "react";
+import Link from "next/link";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
+import { toast } from "react-hot-toast";
+import { useStateContext } from "@/context/StateContext";
+import { urlFor } from "@/lib/client";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } =
+    useStateContext();
 
-export default Cart
+  return (
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your Shopping Bag is empty</h3>
+            <Link href="/">
+              <button type="button" onClick={() => setShowCart(false)}>
+                {" "}
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+
+        <div className="product-container">
+          {cartItems.length >= 1 &&
+            cartItems.map((item, index) => (
+              <div className="product" key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  className="cart-product-image"
+                />
+                <div className="item-desc">
+                  <div className="flex top">
+                    <h5>{item.name}</h5>
+                    <h4>£{item.price}</h4>
+                  </div>
+
+                  <div className="flex bottom">
+                    <div>
+                      <p className="quantity-desc">
+                        <span className="minus" onClick="">
+                          <AiOutlineMinus />
+                        </span>
+                        <span className="num" onClick="">
+                          0
+                        </span>
+                        <span className="plus" onClick="">
+                          <AiOutlinePlus />
+                        </span>
+                      </p>
+                    </div>
+                    <button type="button" className="remove-item" onClick="">
+                      <TiDeleteOutline />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
